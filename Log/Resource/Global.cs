7d29cbd019cc1cfe30d4577e7da87d606f31b673
detached64 <169419571+detached64@users.noreply.github.com>
2024-09-03 00:17:03 +0800

    initial commit

---
+++ Log/Resource/Global.cs
@@ -0,0 +1,37 @@
+﻿// File: Log/Resource/Global.cs
+// Date: 2024/08/25
+// Description: Log窗体的全局设置
+//
+// Copyright (C) 2024 detached64
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log.Resource
+{
+    /// <summary>
+    /// Global settings of Log.
+    /// </summary>
+    internal class Global
+    {
+        internal static bool AutoSaveLogSave = true;
+        internal static bool AutoSaveLogVerbose = true;
+    }
+}
