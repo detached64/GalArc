9c155bb16d9b3cf2fdf6d81a1983bf7f9b40347e
detached64 <169419571+detached64@users.noreply.github.com>
2024-09-17 12:50:16 +0800

    fix nexas old index

--- ArcFormats/NeXAS/PAC.cs
+++ ArcFormats/NeXAS/PAC.cs
@@ -1,5 +1,6 @@
 ﻿using Log;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -19,12 +20,25 @@ namespace ArcFormats.NeXAS
             Zstd = 7
         }
 
+        private struct FileEntry
+        {
+            public string FileName;
+            public string FilePath;
+            public int Offset;
+            public int UnpackedSize;
+            public int PackedSize;
+        }
+        private static string FolderPath = string.Empty;
+
+        private static List<FileEntry> entries = new List<FileEntry>();
+
         private static byte[] signature = { 0x50, 0x41, 0x43 };       //"PAC"
 
         public static void Unpack(string filePath, string folderPath, Encoding encoding)
         {
             FileStream fs = File.OpenRead(filePath);
             BinaryReader reader = new BinaryReader(fs);
+            FolderPath = folderPath;
             if (!reader.ReadBytes(3).SequenceEqual(signature))
             {
                 LogUtility.Error_NotValidArchive();
@@ -33,75 +47,128 @@ namespace ArcFormats.NeXAS
             long fileSize = reader.BaseStream.Length;
             int fileCount = reader.ReadInt32();
             int methodCount = reader.ReadInt32();
-            if (methodCount == 4)           // also zlib
+            switch (methodCount)
             {
-                methodCount = 3;
+                case 4:
+                case 5:
+                    methodCount = 3;
+                    break;
             }
             Method method = (Method)methodCount;
-
-            fs.Seek(-4, SeekOrigin.End);
-            int packedLen = reader.ReadInt32();
-            int unpackedLen = fileCount * 76;
-            fs.Seek(-4 - packedLen, SeekOrigin.End);
-            byte[] packedIndex = Xor.xor(reader.ReadBytes(packedLen), 0xff);
             Directory.CreateDirectory(folderPath);
             LogUtility.InitBar(fileCount);
 
-            byte[] index = Huffman.Decompress(packedIndex, unpackedLen);
-            MemoryStream msIndex = new MemoryStream(index);
-            BinaryReader readerIndex = new BinaryReader(msIndex);
+            ReadOldIndex(reader, fileCount, out bool isSuccess);
+            if (!isSuccess)
+            {
+                entries.Clear();
+                ReadNewIndex(reader, fileCount, out isSuccess);
+            }
+            if (!isSuccess)
+            {
+                LogUtility.Error("Failed to read index.");
+                return;
+            }
 
             for (int i = 0; i < fileCount; i++)
             {
-                string fileName = ArcEncoding.Shift_JIS.GetString(readerIndex.ReadBytes(64)).TrimEnd('\0');
-                int offset = readerIndex.ReadInt32();
-                int unpackedSize = readerIndex.ReadInt32();
-                int packedSize = readerIndex.ReadInt32();
-                string path = folderPath + "\\" + fileName;
+                fs.Seek(entries[i].Offset, SeekOrigin.Begin);
+                byte[] fileData = reader.ReadBytes(entries[i].PackedSize);
 
-                fs.Seek(offset, SeekOrigin.Begin);
-                byte[] fileData = reader.ReadBytes(packedSize);
-
-                if (unpackedSize != packedSize && method != Method.None) // compressed
+                if (entries[i].UnpackedSize != entries[i].PackedSize && method != Method.None) // compressed
                 {
-                    LogUtility.Debug("Packed file detected:" + fileName + ".Try " + method.ToString() + "……");
+                    LogUtility.Debug("Packed file detected:" + entries[i].FileName + ".Try " + method.ToString() + "……");
                     try
                     {
                         switch (method)
                         {
                             case Method.Huffman:
-                                File.WriteAllBytes(path, Huffman.Decompress(fileData, unpackedSize));
+                                File.WriteAllBytes(entries[i].FilePath, Huffman.Decompress(fileData, entries[i].UnpackedSize));
                                 break;
 
                             case Method.Lzss:
-                                File.WriteAllBytes(path, Lzss.Decompress(fileData));
+                                File.WriteAllBytes(entries[i].FilePath, Lzss.Decompress(fileData));
                                 break;
 
                             case Method.Zlib:
-                                File.WriteAllBytes(path, Zlib.DecompressBytes(fileData));
+                                File.WriteAllBytes(entries[i].FilePath, Zlib.DecompressBytes(fileData));
                                 break;
 
                             case Method.Zstd:
-                                File.WriteAllBytes(path, Zstd.Decompress(fileData));
+                                File.WriteAllBytes(entries[i].FilePath, Zstd.Decompress(fileData));
                                 break;
                         }
                     }
                     catch (Exception ex)
                     {
-                        LogUtility.Error("Decompress" + method.ToString() + " failed:" + fileName);
+                        LogUtility.Error("Decompress " + method.ToString() + " failed:" + entries[i].FileName, false);
                         LogUtility.Debug(ex.Message);
                     }
                 }
                 else                                                    // No compression
                 {
-                    File.WriteAllBytes(folderPath + "\\" + fileName, fileData);
+                    File.WriteAllBytes(entries[i].FilePath, fileData);
                 }
                 LogUtility.UpdateBar();
             }
+            entries.Clear();
             fs.Dispose();
-            msIndex.Dispose();
             reader.Dispose();
-            readerIndex.Dispose();
+        }
+
+        private static void ReadOldIndex(BinaryReader reader, int fileCount, out bool isSuccess)
+        {
+            reader.BaseStream.Position = 12;
+            try
+            {
+                for (int i = 0; i < fileCount; i++)
+                {
+                    FileEntry entry = new FileEntry();
+                    entry.FileName = ArcEncoding.Shift_JIS.GetString(reader.ReadBytes(64)).TrimEnd('\0');
+                    entry.FilePath = Path.Combine(FolderPath, entry.FileName);
+                    entry.Offset = reader.ReadInt32();
+                    entry.UnpackedSize = reader.ReadInt32();
+                    entry.PackedSize = reader.ReadInt32();
+                    entries.Add(entry);
+                }
+            }
+            catch
+            {
+                isSuccess = false;
+                return;
+            }
+            isSuccess = true;
+        }
+
+        private static void ReadNewIndex(BinaryReader reader, int fileCount, out bool isSuccess)
+        {
+            reader.BaseStream.Seek(-4, SeekOrigin.End);
+            int packedLen = reader.ReadInt32();
+            int unpackedLen = fileCount * 76;
+            reader.BaseStream.Seek(-4 - packedLen, SeekOrigin.End);
+            byte[] packedIndex = Xor.xor(reader.ReadBytes(packedLen), 0xff);
+            byte[] index = Huffman.Decompress(packedIndex, unpackedLen);
+            MemoryStream msIndex = new MemoryStream(index);
+            BinaryReader readerIndex = new BinaryReader(msIndex);
+            try
+            {
+                for (int i = 0; i < fileCount; i++)
+                {
+                    FileEntry entry = new FileEntry();
+                    entry.FileName = ArcEncoding.Shift_JIS.GetString(readerIndex.ReadBytes(64)).TrimEnd('\0');
+                    entry.FilePath = Path.Combine(FolderPath, entry.FileName);
+                    entry.Offset = readerIndex.ReadInt32();
+                    entry.UnpackedSize = readerIndex.ReadInt32();
+                    entry.PackedSize = readerIndex.ReadInt32();
+                    entries.Add(entry);
+                }
+            }
+            catch
+            {
+                isSuccess = false;
+                return;
+            }
+            isSuccess = true;
         }
     }
 }
