e975593c4dea9706ca8a6dd319c863b08794041c
detached64 <169419571+detached64@users.noreply.github.com>
2024-10-12 18:59:53 +0800

    implement

--- ArcFormats/InnocentGrey/DAT.cs
+++ ArcFormats/InnocentGrey/DAT.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using Utility;
 
@@ -12,7 +11,7 @@ namespace ArcFormats.InnocentGrey
     {
         private struct Header
         {
-            public string magic { get; set; }       //"PACKDAT."
+            public string magic { get; set; }   //"PACKDAT."
             public uint fileCount { get; set; }
             public uint fileCount1 { get; set; }
         }
@@ -22,8 +21,8 @@ namespace ArcFormats.InnocentGrey
             public string fileName { get; set; }
             public uint offset { get; set; }
             public uint fileType { get; set; }
-            public uint fileSize_uncompressed { get; set; }
-            public uint fileSize_compressed { get; set; }
+            public uint unpackedSize { get; set; }
+            public uint packedSize { get; set; }
             public bool isCompressed { get; set; }
         }
 
@@ -31,7 +30,7 @@ namespace ArcFormats.InnocentGrey
         {
             FileStream fs = File.OpenRead(filePath);
             BinaryReader br = new BinaryReader(fs);
-            if (Encoding.ASCII.GetString(br.ReadBytes(7)) != "PACKDAT" || br.ReadByte() != '\x2e')
+            if (Encoding.ASCII.GetString(br.ReadBytes(8)) != "PACKDAT.")
             {
                 LogUtility.ErrorInvalidArchive();
             }
@@ -46,15 +45,16 @@ namespace ArcFormats.InnocentGrey
             LogUtility.InitBar(header.fileCount);
             List<Entry> entries = new List<Entry>();
             Directory.CreateDirectory(folderPath);
+
             for (int i = 0; i < header.fileCount; i++)
             {
                 Entry entry = new Entry();
                 entry.fileName = Encoding.ASCII.GetString(br.ReadBytes(32)).TrimEnd('\0');
                 entry.offset = br.ReadUInt32();
                 entry.fileType = br.ReadUInt32();
-                entry.fileSize_uncompressed = br.ReadUInt32();
-                entry.fileSize_compressed = br.ReadUInt32();
-                entry.isCompressed = entry.fileSize_compressed != entry.fileSize_uncompressed;
+                entry.unpackedSize = br.ReadUInt32();
+                entry.packedSize = br.ReadUInt32();
+                entry.isCompressed = entry.packedSize != entry.unpackedSize;
 
                 if (entry.isCompressed)     //skip compressed data for now
                 {
@@ -66,7 +66,7 @@ namespace ArcFormats.InnocentGrey
             for (int i = 0; i < header.fileCount; i++)
             {
                 fs.Position = entries[i].offset;//have to seek manually,or it will read the wrong data
-                byte[] data = br.ReadBytes((int)entries[i].fileSize_uncompressed);
+                byte[] data = br.ReadBytes((int)entries[i].unpackedSize);
                 byte key = (byte)(Path.GetExtension(entries[i].fileName) == ".s" ? 0xFF : 0);
                 data = Xor.xor(data, key);
                 File.WriteAllBytes(Path.Combine(folderPath, entries[i].fileName), data);
@@ -82,30 +82,31 @@ namespace ArcFormats.InnocentGrey
             BinaryWriter bw = new BinaryWriter(fw);
             DirectoryInfo d = new DirectoryInfo(folderPath);
             Header header = new Header();
-            header.magic = "PACKDAT";
-            header.fileCount = (uint)d.GetFiles("*.*", SearchOption.TopDirectoryOnly).Count();
+            header.magic = "PACKDAT.";
+            string[] files = Directory.GetFiles(folderPath);
+            header.fileCount = (uint)files.Length;
             header.fileCount1 = header.fileCount;
             LogUtility.InitBar(header.fileCount);
             bw.Write(Encoding.ASCII.GetBytes(header.magic));
-            bw.Write('\x2e');
             bw.Write(header.fileCount);
             bw.Write(header.fileCount1);
             uint dataOffset = 16 + header.fileCount * 48;
 
-            foreach (FileInfo file in d.GetFiles("*.*", SearchOption.TopDirectoryOnly))
+            foreach (string file in files)
             {
-                bw.Write(Encoding.ASCII.GetBytes(file.Name.PadRight(32, '\0')));
+                bw.Write(Encoding.ASCII.GetBytes(Path.GetFileName(file).PadRight(32, '\0')));
                 bw.Write(dataOffset);
-                dataOffset += (uint)file.Length;
+                uint size = (uint)new FileInfo(file).Length;
+                dataOffset += size;
                 bw.Write(0x20000000);
-                bw.Write((uint)file.Length);
-                bw.Write((uint)file.Length);
+                bw.Write(size);
+                bw.Write(size);
             }
 
-            foreach (FileInfo file in d.GetFiles("*.*", SearchOption.TopDirectoryOnly))
+            foreach (string file in files)
             {
-                byte[] data = File.ReadAllBytes(file.FullName);
-                byte key = (byte)(Path.GetExtension(file.Name) == ".s" ? 0xFF : 0);
+                byte[] data = File.ReadAllBytes(file);
+                byte key = (byte)(Path.GetExtension(file) == ".s" ? 0xFF : 0);
                 data = Xor.xor(data, key);
                 bw.Write(data);
                 LogUtility.UpdateBar();
