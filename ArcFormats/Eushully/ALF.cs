f49786a6a199f6eca505ede831ccdc6f4abc1e4f
detached64 <169419571+detached64@users.noreply.github.com>
2024-10-28 17:59:06 +0800

    add eushully v4 unpack

--- ArcFormats/Eushully/ALF.cs
+++ ArcFormats/Eushully/ALF.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using Utility.Compression;
+using Utility.Extensions;
 
 namespace ArcFormats.Eushully
 {
@@ -12,6 +13,10 @@ namespace ArcFormats.Eushully
         private static readonly string IndexFileNameV5 = "SYS5INI.BIN";
 
         private static readonly string IndexFileMagicV5 = "S5IC502 ";
+
+        private static readonly string IndexFileNameV4 = "sys4ini.bin";
+
+        private static readonly string IndexFileMagicV4 = "S4IC415 ";
 
         private class Entry
         {
@@ -23,11 +28,108 @@ namespace ArcFormats.Eushully
 
         public void Unpack(string filePath, string folderPath)
         {
-            string indexPath = Path.Combine(Path.GetDirectoryName(filePath), IndexFileNameV5);
-            if (!File.Exists(indexPath))
+            string indexPathV4 = Path.Combine(Path.GetDirectoryName(filePath), IndexFileNameV4);
+            string indexPathV5 = Path.Combine(Path.GetDirectoryName(filePath), IndexFileNameV5);
+            if (File.Exists(indexPathV5))
             {
-                LogUtility.ErrorNeedAnotherFile(IndexFileNameV5);
+                UnpackV5(filePath, folderPath, indexPathV5);
             }
+            else if (File.Exists(indexPathV4))
+            {
+                UnpackV4(filePath, folderPath, indexPathV4);
+            }
+            else
+            {
+                LogUtility.ErrorNeedAnotherFile($"{IndexFileNameV4}/{IndexFileNameV5}");
+            }
+        }
+
+        private static void UnpackV4(string filePath, string folderPath, string indexPath)
+        {
+            byte[] index;
+
+            using (FileStream fs = File.OpenRead(indexPath))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    if (Encoding.ASCII.GetString(br.ReadBytes(8)) != IndexFileMagicV4)
+                    {
+                        LogUtility.ErrorInvalidArchive();
+                    }
+                    fs.Position = 312;
+                    byte[] raw = br.ReadBytes((int)fs.Length - 312);
+                    index = Lzss.Decompress(raw);
+                    raw = null;
+                }
+            }
+            List<string> archives = new List<string>();
+            int archiveIndex;
+            List<Entry> entries = new List<Entry>();
+
+            using (MemoryStream ms = new MemoryStream(index))
+            {
+                using (BinaryReader br = new BinaryReader(ms))
+                {
+                    int pos = 4;
+                    // read archive info
+                    int archiveCount = br.ReadInt32();
+                    for (int i = 0; i < archiveCount; i++)
+                    {
+                        ms.Position = pos;
+                        string name = br.ReadCString(Encoding.ASCII);
+                        archives.Add(name.ToLower());
+                        pos += 256;
+                    }
+                    archiveIndex = archives.IndexOf(Path.GetFileName(filePath).ToLower());
+                    if (archiveIndex == -1)
+                    {
+                        LogUtility.ErrorInvalidArchive();
+                    }
+                    else
+                    {
+                        LogUtility.ShowVersion("alf", 4);
+                        ms.Position = pos;
+                        int fileCount = br.ReadInt32();
+                        for (int i = 0; i < fileCount; i++)
+                        {
+                            Entry entry = new Entry();
+                            entry.Name = Encoding.ASCII.GetString(br.ReadBytes(64)).TrimEnd('\0');
+                            entry.ArchiveIndex = br.ReadInt32();
+                            if (entry.ArchiveIndex != archiveIndex)
+                            {
+                                ms.Position += 12;
+                                continue;
+                            }
+                            ms.Position += 4;
+                            entry.Offset = br.ReadUInt32();
+                            entry.Size = br.ReadUInt32();
+                            entries.Add(entry);
+                        }
+                    }
+                }
+            }
+
+            LogUtility.InitBar(entries.Count);
+            Directory.CreateDirectory(folderPath);
+            using (FileStream fs = File.OpenRead(filePath))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    foreach (Entry entry in entries)
+                    {
+                        fs.Position = entry.Offset;
+                        byte[] buffer = br.ReadBytes((int)entry.Size);
+                        File.WriteAllBytes(Path.Combine(folderPath, entry.Name), buffer);
+                        buffer = null;
+                        LogUtility.UpdateBar();
+                    }
+                }
+            }
+            index = null;
+        }
+
+        private static void UnpackV5(string filePath, string folderPath, string indexPath)
+        {
             byte[] index;
 
             using (FileStream fs = File.OpenRead(indexPath))
@@ -39,7 +141,7 @@ namespace ArcFormats.Eushully
                         LogUtility.ErrorInvalidArchive();
                     }
                     fs.Position = 552;
-                    byte[] raw = br.ReadBytes((int)fs.Length - 532);
+                    byte[] raw = br.ReadBytes((int)fs.Length - 552);
                     index = Lzss.Decompress(raw);
                     raw = null;
                 }
@@ -59,16 +161,17 @@ namespace ArcFormats.Eushully
                     {
                         ms.Position = pos;
                         string name = Encoding.Unicode.GetString(br.ReadBytes(24)).TrimEnd('\0');
-                        archives.Add(name);
+                        archives.Add(name.ToLower());
                         pos += 512;
                     }
-                    archiveIndex = archives.IndexOf(Path.GetFileName(filePath));
+                    archiveIndex = archives.IndexOf(Path.GetFileName(filePath).ToLower());
                     if (archiveIndex == -1)
                     {
                         LogUtility.ErrorInvalidArchive();
                     }
                     else
                     {
+                        LogUtility.ShowVersion("alf", 5);
                         ms.Position = pos;
                         int fileCount = br.ReadInt32();
                         for (int i = 0; i < fileCount; i++)
@@ -106,6 +209,7 @@ namespace ArcFormats.Eushully
                 }
             }
             index = null;
+
         }
     }
 }
