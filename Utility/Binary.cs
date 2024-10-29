ed559285da703bbec50cae3f1c32a62b9a785a28
detached64 <RiegPerrett308@gmail.com>
2024-10-29 14:03:56 +0800

    add seraph dat unpack

--- Utility/Binary.cs
+++ Utility/Binary.cs
@@ -47,5 +47,31 @@ namespace Utility
             count &= 7;
             return (byte)(v >> count | v << (8 - count));
         }
+
+        /// <summary>
+        /// When src and dst overlap and uncopied src is updated by dst in the process, this method copies updated bytes instead of original ones.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="src"></param>
+        /// <param name="dst"></param>
+        /// <param name="count"></param>
+        public static void CopyOverlapped(byte[] data, int src, int dst, int count)
+        {
+            if (dst > src)
+            {
+                while (count > 0)
+                {
+                    int preceding = Math.Min(dst - src, count);
+                    Buffer.BlockCopy(data, src, data, dst, preceding);
+                    dst += preceding;
+                    count -= preceding;
+                }
+            }
+            else
+            {
+                Buffer.BlockCopy(data, src, data, dst, count);
+            }
+        }
+
     }
 }
---
+++ Utility/Lz.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace Utility
+{
+    public class Lz
+    {
+        public static byte[] Decompress(byte[] data)
+        {
+            using (MemoryStream input = new MemoryStream(data))
+            {
+                int unpacked_size = ReadInt32(input);
+                var output = new byte[unpacked_size];
+                int dst = 0;
+                while (dst < unpacked_size)
+                {
+                    int ctl = input.ReadByte();
+                    if (-1 == ctl)
+                    {
+                        throw new EndOfStreamException();
+                    }
+
+                    if (0 != (ctl & 0x80))
+                    {
+                        byte lo = (byte)input.ReadByte();
+                        int offset = ((ctl << 3 | lo >> 5) & 0x3FF) + 1;
+                        int count = (lo & 0x1F) + 1;
+                        Binary.CopyOverlapped(output, dst - offset, dst, count);
+                        dst += count;
+                    }
+                    else
+                    {
+                        int count = ctl + 1;
+                        if (input.Read(output, dst, count) != count)
+                        {
+                            throw new EndOfStreamException();
+                        }
+
+                        dst += count;
+                    }
+                }
+                return output;
+            }
+        }
+        private static int ReadInt32(Stream stream)
+        {
+            byte[] buffer = new byte[4];
+            if (stream.Read(buffer, 0, 4) != 4)
+                throw new EndOfStreamException();
+            return BitConverter.ToInt32(buffer, 0);
+        }
+    }
+}
