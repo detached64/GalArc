7ff5b64c13bdf1c532a37b6827aad9b9ed319213
detached64 <169419571+detached64@users.noreply.github.com>
2024-10-30 18:31:57 +0800

    add extension system

---
+++ GalArc/Extensions/GARbroDB/GARbroDBConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace GalArc.Extensions.GARbroDB
+{
+    public class GARbroDBConfig
+    {
+        public static bool IsGARbroDBEnabled { get; set; } = true;
+
+        public static string GARbroDBPath { get; set; } = Path.Combine(Environment.CurrentDirectory, "Extensions\\GARbroDB\\Formats.json");
+
+    }
+}
