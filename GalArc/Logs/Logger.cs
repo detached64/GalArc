8f2af96423060275e3e04eb0c4fbc6b5e257ddad
detached64 <169419571+detached64@users.noreply.github.com>
2024-12-11 19:26:44 +0800

    implement cached log

--- GalArc/Logs/Logger.cs
+++ GalArc/Logs/Logger.cs
@@ -20,7 +20,9 @@
 
 using GalArc.Properties;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -37,44 +39,91 @@ namespace GalArc.Logs
     {
         public static event EventHandler<string> Process;
         public static event EventHandler<string> ErrorOccured;
-        private static CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
 
-        private static void SaveLog(string log)
+        private static readonly int CacheSize = 12;
+        private static readonly string LogPath = "log.txt";
+
+        private static List<string> _logCache = new List<string>();
+        private static readonly object _lock = new object();
+        private static CancellationTokenSource _cancellationToken = new CancellationTokenSource();
+
+        private static void Append(string msg)
         {
-            File.AppendAllText("log.txt", "[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "]" + log);
+            lock (_lock)
+            {
+                _logCache.Add($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}]{msg}");
+            }
+
+            if (_logCache.Count > CacheSize)
+            {
+                Flush();
+            }
         }
 
-        private static void WriteAndSaveLog(string log, int logLevel = 1)
+        private static void Append(string msg, int level)
+        {
+            lock (_lock)
+            {
+                _logCache.Add($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}][{(LogLevel)level}]{msg}");
+            }
+
+            if (_logCache.Count > CacheSize)
+            {
+                Flush();
+            }
+        }
+
+        public static void Flush(bool isClosing = false)
+        {
+            lock (_lock)
+            {
+                if (_logCache.Count <= 0)
+                {
+                    throw new Exception("Log cache is empty.");
+                }
+                if (!BaseSettings.Default.ToSaveLog)
+                {
+                    _logCache.Clear();
+                    return;
+                }
+
+                File.AppendAllLines(LogPath, _logCache, Encoding.UTF8);
+                _logCache.Clear();
+                if (isClosing)
+                {
+                    using (StreamWriter sw = File.AppendText(LogPath))
+                    {
+                        sw.WriteLine();
+                    }
+                }
+            }
+        }
+
+        private static void WriteAndSaveLog(string msg, int logLevel = 1)
         {
             LogLevel level = (LogLevel)logLevel;
             if ((level == LogLevel.Debug && BaseSettings.Default.IsDebugMode) || level == LogLevel.Info || level == LogLevel.Error)
             {
                 if (LogWindow.Instance.log_txtLog.InvokeRequired)
                 {
-                    LogWindow.Instance.log_txtLog.Invoke(new Action(() => LogWindow.Instance.log_txtLog.AppendText(log + Environment.NewLine)));
+                    LogWindow.Instance.log_txtLog.Invoke(new Action(() => LogWindow.Instance.log_txtLog.AppendText(msg + Environment.NewLine)));
                 }
                 else
                 {
-                    LogWindow.Instance.log_txtLog.AppendText(log + Environment.NewLine);
+                    LogWindow.Instance.log_txtLog.AppendText(msg + Environment.NewLine);
                 }
             }
             if (BaseSettings.Default.ToSaveLog)
             {
-                SaveLog("[" + level.ToString() + "]" + log + Environment.NewLine);
+                Append(msg, logLevel);
             }
         }
 
         public static void NewInstance()
         {
-            File.AppendAllText("log.txt", Environment.NewLine + "[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "][" + Resources.logNewInstance + "]" + Environment.NewLine);
+            Append($"[{Resources.logNewInstance}]");
         }
 
-        /// <summary>
-        /// Show error log and throw exception if <paramref name="toThrow"/> is true.
-        /// </summary>
-        /// <param name="log"></param>
-        /// <param name="toThrow"></param>
-        /// <exception cref="Exception"></exception>
         public static void Error(string log, bool toThrow = true)
         {
             if (!toThrow)
@@ -87,56 +136,66 @@ namespace GalArc.Logs
                 throw new Exception(log);
             }
         }
+
         public static void Info(string log)
         {
             WriteAndSaveLog(log, 1);
         }
+
         public static void Debug(string log)
         {
             WriteAndSaveLog(log, 2);
         }
+
         public static void InfoRevoke(string log)
         {
             WriteAndSaveLog(log, 1);
             OnProcess(log);
         }
+
         public static void DebugRevoke(string log)
         {
             WriteAndSaveLog(log, 2);
             OnProcess(log);
         }
+
         public static void ErrorInvalidArchive()
         {
             throw new Exception(Resources.logErrorNotValidArc);
         }
+
         public static void ErrorNeedAnotherFile(string file)
         {
             throw new Exception(string.Format(Resources.logErrorSpecifiedFileNotFound, file));
         }
+
         public static void ErrorNeedOriginalFile(string file)
         {
             throw new Exception(string.Format(Resources.logErrorOriginalFileNotFound, file));
         }
 
-        public static void InitPack(string inputFolderPath, string outputFilePath)
+        public static void InitPack(string input, string output)
         {
-            Debug(Resources.logInputFolder + "\t" + inputFolderPath);
-            Debug(Resources.logOutputFile + "\t" + outputFilePath);
+            Debug(Resources.logInputFolder + "\t" + input);
+            Debug(Resources.logOutputFile + "\t" + output);
             Info(Resources.logPacking);
-            LogWindow.Instance.bar.Invoke(new Action(() => LogWindow.Instance.bar.Value = 0));
+            SetBarValue(0);
         }
+
         public static void FinishPack()
         {
             LogWindow.Instance.bar.Invoke(new Action(() => LogWindow.Instance.bar.Value = LogWindow.Instance.bar.Maximum));
             InfoRevoke(Resources.logPackFinished);
         }
-        public static void InitUnpack(string inputFilePath, string outputFolderPath)
+
+        public static void InitUnpack(string input, string output)
         {
-            Debug(Resources.logInputFile + "\t" + inputFilePath);
-            Debug(Resources.logOutputFolder + "\t" + outputFolderPath);
+            Debug(Resources.logInputFile + "\t" + input);
+            Debug(Resources.logOutputFolder + "\t" + output);
             Info(Resources.logUnpacking);
-            LogWindow.Instance.bar.Invoke(new Action(() => LogWindow.Instance.bar.Value = 0));
+            SetBarValue(0);
         }
+
         public static void FinishUnpack()
         {
             LogWindow.Instance.bar.Invoke(new Action(() => LogWindow.Instance.bar.Value = LogWindow.Instance.bar.Maximum));
@@ -147,16 +206,18 @@ namespace GalArc.Logs
         {
             Process?.Invoke(null, message);
         }
+
         internal static void OnErrorOccur(string message)
         {
             ErrorOccured?.Invoke(null, message);
         }
+
         internal static async Task OnShowAndDisappear(string message, int second = 5)
         {
-            _cancellationTokenSource.Cancel();
-            _cancellationTokenSource.Dispose();
-            _cancellationTokenSource = new CancellationTokenSource();
-            var token = _cancellationTokenSource.Token;
+            _cancellationToken.Cancel();
+            _cancellationToken.Dispose();
+            _cancellationToken = new CancellationTokenSource();
+            var token = _cancellationToken.Token;
 
             try
             {
@@ -177,24 +238,29 @@ namespace GalArc.Logs
         {
             InitBar(max, 1);
         }
+
         public static void InitBar(uint max)
         {
             InitBar(max, 1);
         }
+
         public static void InitBar(int max, int m)
         {
             LogWindow.Instance.bar.Invoke(new Action(() => LogWindow.Instance.bar.Maximum = m * max));
             Debug(string.Format(Resources.logFileCountInside, max));
         }
+
         public static void InitBar(uint max, int m)
         {
             LogWindow.Instance.bar.Invoke(new Action(() => LogWindow.Instance.bar.Maximum = m * (int)max));
             Debug(string.Format(Resources.logFileCountInside, max));
         }
+
         public static void UpdateBar()
         {
             LogWindow.Instance.bar.Invoke(new Action(() => LogWindow.Instance.bar.PerformStep()));
         }
+
 
         public static void SetBarValue(int value)
         {
@@ -215,6 +281,7 @@ namespace GalArc.Logs
         {
             InfoRevoke(Resources.logUpdating);
         }
+
         public static async void ShowCheckSuccess(bool existNewer)
         {
             if (existNewer)
@@ -228,10 +295,12 @@ namespace GalArc.Logs
                 await OnShowAndDisappear(Resources.logNoUpdate);
             }
         }
+
         public static void ShowCheckError()
         {
             Error(Resources.logUpdateError, false);
         }
+
         public static void ShowProgramVersion(string cv, string lv)
         {
             Debug(string.Format(Resources.logVersions, cv, lv));
