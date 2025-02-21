896950562b1219c45b376f641d8543716bc99f2b
detached64 <169419571+detached64@users.noreply.github.com>
2025-02-21 23:28:30 +0800

    Implement Majiro package & scripts encryption

---
+++ ArcFormats/Majiro/PackARCOptions.cs
@@ -0,0 +1,29 @@
+﻿using GalArc.Controls;
+using System;
+
+namespace ArcFormats.Majiro
+{
+    public partial class PackARCOptions : OptionsTemplate
+    {
+        internal static bool EncryptScripts = true;
+
+        private readonly string Versions = "1/2/3";
+
+        public PackARCOptions()
+        {
+            InitializeComponent();
+            this.combVersion.Items.AddRange(Versions.Split('/'));
+            this.combVersion.SelectedIndex = 0;
+        }
+
+        private void combVersion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Version = this.combVersion.Text;
+        }
+
+        private void chkbxEncScr_CheckedChanged(object sender, EventArgs e)
+        {
+            EncryptScripts = this.chkbxEncScr.Checked;
+        }
+    }
+}
---
+++ ArcFormats/Majiro/UnpackARCOptions.cs
@@ -0,0 +1,20 @@
+﻿using GalArc.Controls;
+using System;
+
+namespace ArcFormats.Majiro
+{
+    public partial class UnpackARCOptions : OptionsTemplate
+    {
+        internal static bool DecryptScripts = true;
+
+        public UnpackARCOptions()
+        {
+            InitializeComponent();
+        }
+
+        private void chkbxDecScr_CheckedChanged(object sender, EventArgs e)
+        {
+            DecryptScripts = chkbxDecScr.Checked;
+        }
+    }
+}
