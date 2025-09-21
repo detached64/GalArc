03b294a3647e415bafb13b71e645e546fea6c61d
detached64 <169419571+detached64@users.noreply.github.com>
2025-09-21 11:35:51 +0800

    Add ScrollToEnd

--- GalArc/Views/StatusView.axaml.cs
+++ GalArc/Views/StatusView.axaml.cs
@@ -1,5 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Threading;
 using System;
+using System.Collections.Specialized;
 using System.ComponentModel;
 
 namespace GalArc.Views;
@@ -10,6 +12,17 @@ public partial class StatusView : Window
     {
         InitializeComponent();
         Closing += OnClosing;
+
+        LogList.ItemsView.CollectionChanged += (s, e) =>
+        {
+            if (e.Action != NotifyCollectionChangedAction.Add)
+                return;
+            Dispatcher.UIThread.Post(() =>
+            {
+                ScrollViewer sv = LogList.Scroll as ScrollViewer;
+                sv?.ScrollToEnd();
+            });
+        };
     }
 
     private void OnClosing(object sender, CancelEventArgs e)
