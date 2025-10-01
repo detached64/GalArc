81910e2e216bd5734d8ab3ad1198e27a8ac40362
detached64 <169419571+detached64@users.noreply.github.com>
2025-10-02 00:49:12 +0800

    Add Strings

--- GalArc/I18n/GuiStrings.Designer.cs
+++ GalArc/I18n/GuiStrings.Designer.cs
@@ -322,6 +322,15 @@ namespace GalArc.I18n {
         }
         
         /// <summary>
+        ///   查找类似 Hash Version 的本地化字符串。
+        /// </summary>
+        public static string HashVersion {
+            get {
+                return ResourceManager.GetString("HashVersion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   查找类似 Help 的本地化字符串。
         /// </summary>
         public static string Help {
