f8db7c0368d6ee7b609094282f2a7ed3423c82bf
detached64 <169419571+detached64@users.noreply.github.com>
2025-12-26 21:50:52 +0800

    feat(formats): add customizable seed for NekoPack DAT v2 packing

---
+++ GalArc/Models/Formats/NekoPack/NekoPackDATV2UnpackWidget.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GalArc.Models.Formats.NekoPack;
+
+public partial class NekoPackDATV2UnpackWidget : UserControl
+{
+    public NekoPackDATV2UnpackWidget()
+    {
+        InitializeComponent();
+    }
+}
